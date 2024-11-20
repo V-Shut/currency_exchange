a2d660276150c165ae0527db96da00cc327bfe91
Vitalii <shut.vitaliy.ol@gmail.com>
2024-11-20 11:53:05 +0200

    add fetch and calculation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type Currency = {
+  r030: number;
+  txt: string;
+  rate: number;
+  cc: string;
+  exchangedate: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currencies, setCurrencies] = useState<Currency[]>([]);
+  const [leftInput, setLeftInput] = useState<number | string>(0);
+  const [rightInput, setRightInput] = useState<number | string>(0);
+  const [leftSelect, setLeftSelect] = useState<Currency | null>(null);
+  const [rightSelect, setRightSelect] = useState<Currency | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      let data = await response.json();
+      data = data.sort((a: Currency, b: Currency) =>
+        a.txt.localeCompare(b.txt)
+      );
+      setCurrencies(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  const handleLeftSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCurrency = currencies.find(
+      (currency) => currency.cc === event.target.value
+    );
+    if (selectedCurrency) {
+      setLeftSelect(selectedCurrency);
+    }
+  };
+
+  const handleRightSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCurrency = currencies.find(
+      (currency) => currency.cc === event.target.value
+    );
+    if (selectedCurrency) {
+      setRightSelect(selectedCurrency);
+    }
+  };
+
+  useEffect(() => {
+    if (leftSelect && rightSelect) {
+      setRightInput(
+        (Number(leftInput) * leftSelect.rate) / rightSelect.rate
+      );
+    }
+  }, [leftInput, leftSelect, rightSelect]);
+
+  useEffect(() => {
+    if (leftSelect && rightSelect) {
+      setLeftInput(
+        (Number(rightInput) * rightSelect.rate) / leftSelect.rate
+      );
+    }
+  }, [rightInput, leftSelect, rightSelect]);
+
+  useEffect(() => {
+    setLeftSelect(currencies[0]);
+    setRightSelect(currencies[0]);
+  }, [currencies]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="curr_container">
+          <input
+            className="curr_input"
+            type="text"
+            value={leftInput}
+            onChange={(event) => setLeftInput(event.target.value)}
+          />
+          <select
+            className="curr_select"
+            name="left select"
+            onChange={handleLeftSelect}
+            value={leftSelect?.cc || ""}
+          >
+            {currencies.map((el) => (
+              <option key={el.cc} value={el.cc}>
+                {el.txt}, {el.cc}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="curr_container">
+          <input
+            className="curr_input"
+            type="text"
+            value={rightInput}
+            onChange={(event) => setRightInput(event.target.value)}
+          />
+          <select
+            className="curr_select"
+            name="right select"
+            onChange={handleRightSelect}
+            value={rightSelect?.cc || ""}
+          >
+            {currencies.map((el) => (
+              <option key={el.cc} value={el.cc}>
+                {el.txt}, {el.cc}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
