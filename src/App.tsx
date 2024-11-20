fc12f5aa8ead93ca6c25445dae0cb54f25a0589a
Vitalii <shut.vitaliy.ol@gmail.com>
2024-11-20 12:50:18 +0200

    fix rerender bug, create design

--- src/App.tsx
+++ src/App.tsx
@@ -2,129 +2,138 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 type Currency = {
-  r030: number;
-  txt: string;
-  rate: number;
-  cc: string;
-  exchangedate: string;
+	r030: number;
+	txt: string;
+	rate: number;
+	cc: string;
+	exchangedate: string;
 };
 
 function App() {
-  const [currencies, setCurrencies] = useState<Currency[]>([]);
-  const [leftInput, setLeftInput] = useState<number | string>(0);
-  const [rightInput, setRightInput] = useState<number | string>(0);
-  const [leftSelect, setLeftSelect] = useState<Currency | null>(null);
-  const [rightSelect, setRightSelect] = useState<Currency | null>(null);
+	const [currencies, setCurrencies] = useState<Currency[]>([]);
+	const [leftInput, setLeftInput] = useState<number | string>(0);
+	const [rightInput, setRightInput] = useState<number | string>(0);
+	const [leftSelect, setLeftSelect] = useState<Currency | null>(null);
+	const [rightSelect, setRightSelect] = useState<Currency | null>(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
-      );
+	const fetchData = async () => {
+		try {
+			const response = await fetch(
+				"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+			);
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
 
-      let data = await response.json();
-      data = data.sort((a: Currency, b: Currency) =>
-        a.txt.localeCompare(b.txt)
-      );
-      setCurrencies(data);
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  };
+			const data = await response.json();
+			data.push({
+				r030: 0,
+				txt: "Українська гривня",
+				rate: 1,
+				cc: "UAH",
+				exchangedate: data[0].exchangedate,
+			});
+			data.sort((a: Currency, b: Currency) => a.txt.localeCompare(b.txt));
+			setCurrencies(data);
+		} catch (error) {
+			console.error("Fetch error:", error);
+		}
+	};
 
-  const handleLeftSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedCurrency = currencies.find(
-      (currency) => currency.cc === event.target.value
-    );
-    if (selectedCurrency) {
-      setLeftSelect(selectedCurrency);
-    }
-  };
+	const handleLeftSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedCurrency = currencies.find(
+			(currency) => currency.cc === event.target.value
+		);
+		if (selectedCurrency) {
+			setLeftSelect(selectedCurrency);
+		}
+	};
 
-  const handleRightSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedCurrency = currencies.find(
-      (currency) => currency.cc === event.target.value
-    );
-    if (selectedCurrency) {
-      setRightSelect(selectedCurrency);
-    }
-  };
+	const handleRightSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedCurrency = currencies.find(
+			(currency) => currency.cc === event.target.value
+		);
+		if (selectedCurrency) {
+			setRightSelect(selectedCurrency);
+		}
+	};
 
-  useEffect(() => {
-    if (leftSelect && rightSelect) {
-      setRightInput(
-        (Number(leftInput) * leftSelect.rate) / rightSelect.rate
-      );
-    }
-  }, [leftInput, leftSelect, rightSelect]);
+	useEffect(() => {
+		setLeftInput(0);
+	}, [leftSelect, rightSelect]);
 
-  useEffect(() => {
-    if (leftSelect && rightSelect) {
-      setLeftInput(
-        (Number(rightInput) * rightSelect.rate) / leftSelect.rate
-      );
-    }
-  }, [rightInput, leftSelect, rightSelect]);
+	useEffect(() => {
+		if (leftSelect && rightSelect) {
+			const result = (Number(leftInput) * leftSelect.rate) / rightSelect.rate;
+			setRightInput(Number(result.toFixed(0)));
+		}
+	}, [leftInput]);
 
-  useEffect(() => {
-    setLeftSelect(currencies[0]);
-    setRightSelect(currencies[0]);
-  }, [currencies]);
+	useEffect(() => {
+		if (leftSelect && rightSelect) {
+			const result = (Number(rightInput) * rightSelect.rate) / leftSelect.rate;
+			setLeftInput(Number(result.toFixed(0)));
+		}
+	}, [rightInput]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		setLeftSelect(currencies[0]);
+		setRightSelect(currencies[0]);
+	}, [currencies]);
 
-  return (
-    <>
-      <div className="container">
-        <div className="curr_container">
-          <input
-            className="curr_input"
-            type="text"
-            value={leftInput}
-            onChange={(event) => setLeftInput(event.target.value)}
-          />
-          <select
-            className="curr_select"
-            name="left select"
-            onChange={handleLeftSelect}
-            value={leftSelect?.cc || ""}
-          >
-            {currencies.map((el) => (
-              <option key={el.cc} value={el.cc}>
-                {el.txt}, {el.cc}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="curr_container">
-          <input
-            className="curr_input"
-            type="text"
-            value={rightInput}
-            onChange={(event) => setRightInput(event.target.value)}
-          />
-          <select
-            className="curr_select"
-            name="right select"
-            onChange={handleRightSelect}
-            value={rightSelect?.cc || ""}
-          >
-            {currencies.map((el) => (
-              <option key={el.cc} value={el.cc}>
-                {el.txt}, {el.cc}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-    </>
-  );
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<div className="container">
+				<div className="curr_container">
+					<input
+						className="curr_input"
+						type="text"
+						value={leftInput}
+						onChange={(event) => setLeftInput(event.target.value)}
+					/>
+					<select
+						className="curr_select"
+						name="left select"
+						onChange={handleLeftSelect}
+						value={leftSelect?.cc || ""}>
+						{currencies.map((el) => (
+							<option
+								key={el.cc}
+								value={el.cc}>
+								{el.txt}, {el.cc}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="curr_container">
+					<input
+						className="curr_input"
+						type="text"
+						value={rightInput}
+						onChange={(event) => setRightInput(event.target.value)}
+					/>
+					<select
+						className="curr_select"
+						name="right select"
+						onChange={handleRightSelect}
+						value={rightSelect?.cc || ""}>
+						{currencies.map((el) => (
+							<option
+								key={el.cc}
+								value={el.cc}>
+								{el.txt}, {el.cc}
+							</option>
+						))}
+					</select>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
